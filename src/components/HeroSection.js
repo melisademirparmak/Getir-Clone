8f3190eccfa99187f9dd42499d93d4360e8f0c53
melisademirparmak <melisademirparmaak@gmail.com>
2021-09-29 23:14:31 +0300

    Form

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
+import ReactFlagsSelect from 'react-flags-select';
 
 const HeroSection = () => {
+  const [selected, setSelected] = useState('TR');
+
+  const phones = {
+    US: '+1',
+    DE: '+50',
+    TR: '+90',
+    IT: '+7',
+    IN: '+15',
+  };
+
   const settings = {
     dots: false,
     infinite: true,
@@ -11,23 +22,58 @@ const HeroSection = () => {
     slidesToScroll: 1,
   };
   return (
-    <div className="relative before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
+    <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
       <Slider {...settings}>
         <div>
           <img
-            className="w-full h-[600px] object-cover"
+            className="w-full h-[500px] object-cover"
             src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg"
             alt="Getir 1"
           />
         </div>
         <div>
           <img
-            className="w-full h-[600px]"
+            className="w-full h-[500px]"
             src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg"
             alt="Getir 2"
           />
         </div>
       </Slider>
+      <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
+        <div>
+          <img
+            src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg"
+            alt="Getir"
+          />
+          <h3 className=" mt-10 text-4xl font-semibold text-white">
+            Dakikalar içinde <br /> kapınızda
+          </h3>
+        </div>
+        <div className="w-[400px] rounded-lg bg-gray-50 p-6">
+          <h4 className="text-primary-brand-color text-center font-semibold mb-4">
+            Giriş yap veya kayıt ol
+          </h4>
+          <div className="flex gap-x-2">
+            <ReactFlagsSelect
+              countries={Object.keys(phones)}
+              customLabels={phones}
+              onSelect={(code) => setSelected(code)}
+              selected={selected}
+              className="flag-select"
+            />
+            <label className="flex-1 relative block">
+              <input
+                required
+                className="h-14 px-4 border-2 border-gray-200 rounded w-full transition-colors hover:border-primary-brand-color outline-none focus:border-primary-brand-color peer text-sm pt-2"
+              />
+              <span className="absolute top-0 left-0 h-full px-4 flex items-center text-gray-500 text-sm  peer-focus:h-7  peer-focus:text-primary-brand-color peer-focus:text-xs peer-valid:h-7  peer-valid:text-primary-brand-color peer-valid:text-xs transition-all">
+                Telefon Numarası
+              </span>
+            </label>
+          </div>
+          <button>Telefon numarası ile devam et</button>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundColor: (theme) => ({
+      colors: {
         'brand-color': '#4c3398',
         'primary-brand-color': '#5d3ebc',
         'secondary-brand-color': '#7849f7',
-      }),
-      gradientColorStops: (theme) => ({
-        'brand-color': '#4c3398',
-        'primary-brand-color': '#5d3ebc',
-        'secondary-brand-color': '#7849f7',
-      }),
+      },
     },
   },
   variants: {
