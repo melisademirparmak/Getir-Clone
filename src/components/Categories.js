08d3a36f8188250e7828947a7434831c55fc36ad
melisademirparmak <melisademirparmaak@gmail.com>
2021-10-05 00:35:48 +0300

    Category finish

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import categoryData from 'api/categories.json';
+import { Category } from 'components/ui/Category';
 
 const Categories = () => {
-  return <div>Cattegories</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    setCategories(categoryData);
+  }, []);
+  return (
+    <div className="bg-white py-4">
+      <div className="container mx-auto">
+        <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
+        <div className="grid grid-cols-10">
+          {categories &&
+            categories.map((category, index) => (
+              <Category key={index} category={category} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
---
+++ src/components/ui/Category.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Category = ({ category: { id, title, image } }) => {
+  return (
+    <a
+      href="/"
+      className="flex flex-col group items-center gap-y-2 text-center p-4 transition hover:bg-purple-50"
+    >
+      <img
+        src={image}
+        alt={title}
+        className="w-12 h-12 rounded border border-gray-200"
+      />
+      <span className="text-sm font-semibold text-gray-700 group-hover:text-brand-color tracking-tight">
+        {title}
+      </span>
+    </a>
+  );
+};
