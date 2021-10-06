1a1ea6ce1624539150d1b104ee2c4dafe2610dd4
melisademirparmak <melisademirparmaak@gmail.com>
2021-10-06 16:14:27 +0300

    Favorites

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,8 +1,26 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Title from 'components/ui/Title';
+import Products from 'api/products.json';
+import ProductItem from 'components/ui/ProductItem';
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(Products);
+  }, []);
+
+  return (
+    <div>
+      <Title>Favoriler</Title>
+      <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
+        {products.length &&
+          products.map((product) => (
+            <ProductItem key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Favorites;
---
+++ src/components/ui/ProductItem.js
@@ -0,0 +1,19 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const ProductItem = ({ product }) => {
+  return (
+    <div className="bg-white flex flex-col items-center text-center text-sm gap-y-1 p-5">
+      <picture className="relative block">
+        <button className="w-8 h-8 flex absolute top-0 right-2 border shadow-md rounded-lg border-gray-200 text-purple-700 transition-colors bg-white hover:bg-purple-50 items-center justify-center">
+          <AiOutlinePlus size={16} />
+        </button>
+        <img src={product.image} className="w-full" />
+      </picture>
+      <div className=" font-semibold text-brand-color ">₺{product.price}</div>
+      <div className=" font-bold text-gray-700">{product.title}</div>
+      <div className="text-gary-500">{product.alt}</div>
+    </div>
+  );
+};
+
+export default ProductItem;
