12c09e1651b827f9b15b7e17dc35c89b9028a996
melisademirparmak <melisademirparmaak@gmail.com>
2021-10-25 12:19:07 +0300

    Responsive and useWindowSize

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -13,7 +13,7 @@ const Favorites = () => {
   return (
     <div>
       <Title>Favoriler</Title>
-      <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
+      <div className="grid grid-cols-3 2xl:grid-cols-9 xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 gap-0.1 rounded-lg overflow-hidden">
         {products.length &&
           products.map((product) => (
             <ProductItem key={product.id} product={product} />
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 import ReactFlagsSelect from 'react-flags-select';
 import { FaFacebook } from 'react-icons/fa';
+import { useWindowWidth } from '@react-hook/window-size';
 
 const HeroSection = () => {
   const [selected, setSelected] = useState('TR');
@@ -27,7 +28,7 @@ const HeroSection = () => {
     cssEase: 'linear',
   };
   return (
-    <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
+    <div className="relative  h-auto md:h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
       <Slider {...settings}>
         <div>
           <img
@@ -45,16 +46,16 @@ const HeroSection = () => {
         </div>
       </Slider>
       <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
-        <div>
+        <div className="hidden md:block">
           <img
             src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg"
             alt="Getir"
           />
-          <h3 className=" mt-10 text-4xl font-semibold text-white">
+          <h3 className=" mt-10 text-2xl font-semibold text-white">
             Dakikalar içinde <br /> kapınızda
           </h3>
         </div>
-        <div className="w-[400px] rounded-lg bg-gray-50 p-6">
+        <div className="w-full  md:w-[400px] rounded-lg bg-gray-50 p-6">
           <h4 className="text-primary-brand-color text-center font-semibold mb-4">
             Giriş yap veya kayıt ol
           </h4>
